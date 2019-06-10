3efbbca4a48df48c136c6eaeeee4ba18dd12466b
Raul Mordillo <raul.mordillo.sge@mango.com>
2019-06-10 18:19:40 +0200

    test score all ones

--- src/test/java/BowlingGameShould.java
+++ src/test/java/BowlingGameShould.java
@@ -1,6 +1,5 @@
+import org.junit.Before;
 import org.junit.Test;
-
-import java.util.Calendar;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -8,21 +7,36 @@ import static org.junit.Assert.assertNotNull;
 public class BowlingGameShould {
 
 
+    private Game game;
+
+    @Before
+    public void setUp() throws Exception {
+        game = new Game();
+    }
+
     @Test
     public void create_game() {
 
-        Game game = new Game();
         assertNotNull(game);
     }
 
     @Test
     public void return_score_zero_for_a_gutter_game() {
 
-        Game game = new Game();
-        for(int i = 0; i<21; i++){
-        game.roll(0);
+        for (int i = 0; i < 20; i++) {
+            game.roll(0);
 
         }
         assertEquals(0, game.score());
+    }
+
+    @Test
+    public void return_score_20_for_all_ones() {
+
+        for (int i = 0; i < 20; i++) {
+            game.roll(1);
+
+        }
+        assertEquals(20, game.score());
     }
 }

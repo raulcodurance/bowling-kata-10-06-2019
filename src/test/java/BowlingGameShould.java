df45b8a4fb1f8f363d4c3abeeaa407ca661d14af
Raul Mordillo <raul.mordillo.sge@mango.com>
2019-06-10 18:08:10 +0200

    test gutter game

--- src/test/java/BowlingGameShould.java
+++ src/test/java/BowlingGameShould.java
@@ -1,5 +1,8 @@
 import org.junit.Test;
 
+import java.util.Calendar;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class BowlingGameShould {
@@ -10,5 +13,16 @@ public class BowlingGameShould {
 
         Game game = new Game();
         assertNotNull(game);
+    }
+
+    @Test
+    public void return_score_zero_for_a_gutter_game() {
+
+        Game game = new Game();
+        for(int i = 0; i<21; i++){
+        game.roll(0);
+
+        }
+        assertEquals(0, game.score());
     }
 }

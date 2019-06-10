92cd259fc4fa3b48cd5ce4cd273c071867600507
Raul Mordillo <raul.mordillo.sge@mango.com>
2019-06-10 18:41:43 +0200

    refactor misplaced responsibility

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,12 +1,17 @@
 public class Game {
-    private int score =0;
+    private int score = 0;
+    private int[] rolls = new int[21];
+    private int currentRoll = 0;
 
     public void roll(int pins) {
 
-        this.score += pins;
+        this.rolls[currentRoll++] = pins;
     }
 
     public int score() {
+        for(int i =0; i<21;i++){
+            this.score +=rolls[i];
+        }
         return score;
     }
 }
--- src/test/java/BowlingGameShould.java
+++ src/test/java/BowlingGameShould.java
@@ -1,4 +1,5 @@
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -16,27 +17,39 @@ public class BowlingGameShould {
 
     @Test
     public void create_game() {
-
         assertNotNull(game);
     }
 
     @Test
     public void return_score_zero_for_a_gutter_game() {
-
-        for (int i = 0; i < 20; i++) {
-            game.roll(0);
-
-        }
+        int rolls = 20;
+        int pins = 0;
+        rollMany(rolls, pins);
         assertEquals(0, game.score());
     }
 
+
     @Test
     public void return_score_20_for_all_ones() {
+        rollMany(20, 1);
+        assertEquals(20, game.score());
+    }
 
-        for (int i = 0; i < 20; i++) {
-            game.roll(1);
+    @Ignore
+    @Test
+    public void return_score_16_for_a_spare() {
+        game.roll(5);
+        game.roll(5); // spare
+        game.roll(3);
+
+        rollMany(17, 0);
+        assertEquals(16, game.score());
+    }
+
+    private void rollMany(int rolls, int pins) {
+        for (int i = 0; i < rolls; i++) {
+            game.roll(pins);
 
         }
-        assertEquals(20, game.score());
     }
 }
